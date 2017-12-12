5d701b63f16c9496dd2cd735edcc819ca7037d29
Dustin Ingram <di@users.noreply.github.com>
2017-12-12 17:03:00 -0600

    Add more comments to setup.py

--- setup.py
+++ setup.py
@@ -19,14 +19,28 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
-# define test-dependencies (will be used twice):
-test_deps = ['flake8']
+# Arguments marked as "Required" below must be included for upload to PyPI.
+# Fields marked as "Optional" may be commented out.
 
 setup(
-    name='sampleproject',
+    # This is the name of your project. The first time you publish this
+    # package, this name will be registered for you. It will determine how
+    # users can install this project, e.g.:
+    #
+    # $ pip install sampleproject
+    #
+    # And where it will live on PyPI: https://pypi.org/project/sampleproject/
+    #
+    # There are some restrictions on what makes a valid project name
+    # specification here:
+    # https://packaging.python.org/specifications/core-metadata/#name
+    name='sampleproject',  # Required
 
-    # Versions should comply with PEP440.  For a discussion on single-sourcing
-    # the version across setup.py and the project code, see
+    # Versions should comply with PEP 440:
+    # https://www.python.org/dev/peps/pep-0440/
+    #
+    # For a discussion on single-sourcing the version across setup.py and the
+    # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
     version='0.1',
 
@@ -67,54 +81,68 @@ setup(
         'Programming Language :: Python :: 3.6',
     ],
 
-    # What does your project relate to?
-    keywords='python package template',
+    # This field adds keywords for your project which will appear on the
+    # project page. What does your project relate to?
+    #
+    # Note that this is a string of words separated by whitespace, not a list.
+    keywords='sample setuptools development',  # Optional
 
-    # You can just specify the packages manually here if your project is
+    # You can just specify package directories manually here if your project is
     # simple. Or you can use find_packages().
-    packages=find_packages(exclude=['docs', 'tests']),
-
-    # Test are in subdir tests/
-    test_suite='tests',
-    tests_require=test_deps,
-
-    # Alternatively, if you want to distribute just a my_module.py, uncomment
-    # this:
+    #
+    # Alternatively, if you just want to distribute a single Python file,
+    # uncomment this, which will expect a file called `my_module.py` to exist:
     #   py_modules=["my_module"],
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),  # Required
 
-    # List run-time dependencies here.  These will be installed by pip when
-    # your project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
+    # This field lists other packages that your project depends on to run.
+    # Any package you put here will be installed by pip when your project is
+    # installed, so they must be valid existing projects.
+    #
+    # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=['numpy', 'plumbum'],
 
     # List additional groups of dependencies here (e.g. development
-    # dependencies). You can install these using the following syntax,
-    # for example:
-    # $ pip install -e .[dev,test]
-    extras_require={
+    # dependencies). Users will be able to install these using the "extras"
+    # syntax, for example:
+    #
+    #   $ pip install sampleproject[dev]
+    #
+    # Similar to `install_requires` above, these must be valid existing
+    # projects.
+    extras_require={  # Optional
         'dev': ['check-manifest'],
         'test': ['pytest', 'twine', 'check-manifest', 'readme_renderer',
                  'flake8', 'coverage'],
     },
 
     # If there are data files included in your packages that need to be
-    # installed, specify them here.  If using Python 2.6 or less, then these
-    # have to be included in MANIFEST.in as well.
-    package_data={
-        'sampleproject': ['package_data.dat'],
+    # installed, specify them here.
+    #
+    # If using Python 2.6 or less, then these have to be included in
+    # MANIFEST.in as well.
+    package_data={  # Optional
+        'sample': ['package_data.dat'],
     },
 
     # Although 'package_data' is the preferred approach, in some case you may
     # need to place data files outside of your packages. See:
-    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
+    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files
+    #
     # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    data_files=[('sampleproject_data', ['data/data_file'])],
+    data_files=[('my_data', ['data/data_file'])],  # Optional
 
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
-    # pip to create the appropriate form of executable for the target platform.
-    entry_points={
-        'console_scripts': ['clt=sampleproject.cli:main'],
+    # `pip` to create the appropriate form of executable for the target
+    # platform.
+    #
+    # For example, the following would provide a command called `sample` which
+    # executes the function `main` from this package when invoked:
+    entry_points={  # Optional
+        'console_scripts': [
+            'sample=sample:main',
+        ],
     },
 )
