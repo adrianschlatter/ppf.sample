9131de26471667568cf5b415a328de230a2972e8
방성범 (Bang Seongbeom) <bangseongbeom@hotmail.com>
2018-08-28 21:58:10 +0900

    Make description optional
    `description` keyword corresponds to "Summary" metadata field. "Summary" is not
    a required field as described below:
    
    https://packaging.python.org/specifications/core-metadata/
    > `Metadata-Version`
    > `Name`
    > `Version`
    > All the other fields are optional.

--- setup.py
+++ setup.py
@@ -45,7 +45,22 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1',
+    version='1.2.0',  # Required
+
+    # This is a one-line description or tagline of what your project does. This
+    # corresponds to the "Summary" metadata field:
+    # https://packaging.python.org/specifications/core-metadata/#summary
+    description='A sample Python project',  # Optional
+
+    # This is an optional longer description of your project that represents
+    # the body of text which users will see when they visit PyPI.
+    #
+    # Often, this is the same as your README, so you can just read it in from
+    # that file directly (as we have already done above)
+    #
+    # This field corresponds to the "Description" metadata field:
+    # https://packaging.python.org/specifications/core-metadata/#description-optional
+    long_description=long_description,  # Optional
 
     # Denotes that our long_description is in Markdown; valid values are
     # text/plain, text/x-rst, and text/markdown
