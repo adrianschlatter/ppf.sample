91fbbccbb472ab0dcddf975163f963891892b714
Adrian Schlatter <10478149+adrianschlatter@users.noreply.github.com>
2023-05-06 18:01:29 +0200

    use __version__
    * package defines __version__ attribute.
    * setup.cfg refers to this __version__ attribute.

--- src/ppf/sample/__init__.py
+++ src/ppf/sample/__init__.py
@@ -7,6 +7,8 @@ Sample
 Sample is a package demonstrating how to package Python code
 """
 
+__version__ = '3.0'
+
 # import every function, class, etc. that should be visible in the package
 from .module import *
 
