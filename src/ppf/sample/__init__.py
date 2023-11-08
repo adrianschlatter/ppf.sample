8403b25140afcc14c7dc184a0d1c74cf6a9b0402
Adrian Schlatter <10478149+adrianschlatter@users.noreply.github.com>
2023-11-08 21:08:29 +0100

    setuptools-scm
    * use setuptools-scm to automatically derive package version from git
      tags
    * also, use setuptools-scm because it seems safer. As soon as you have
      it installed, it seems to start interfering with your packaging.
      Therefore, better use it on purpose than by accident.

--- src/ppf/sample/__init__.py
+++ src/ppf/sample/__init__.py
@@ -6,8 +6,12 @@ Sample
 
 Sample is a package demonstrating how to package Python code
 """
+try:
+    from importlib.metadata import version
+except ImportError:
+    from importlib_metadata import version
 
-__version__ = '3.0'
+__version__ = version('ppf-sample')
 
 # import every function, class, etc. that should be visible in the package
 from .module import *
