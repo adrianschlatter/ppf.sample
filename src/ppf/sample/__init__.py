d8ea6356238e1667ce06cb346e0d606b27f75801
Adrian Schlatter <10478149+adrianschlatter@users.noreply.github.com>
2026-01-26 09:54:11 +0100

    fix import error in python3.8 and 3.9

--- src/ppf/sample/__init__.py
+++ src/ppf/sample/__init__.py
@@ -7,9 +7,9 @@ Sample is a package demonstrating how to package Python code
 """
 
 try:
-    from importlib.metadata import version
-except ImportError:
     from importlib_metadata import version
+except ImportError:
+    from importlib.metadata import version
 
 
 __version__ = version(__name__)
